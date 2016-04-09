5d198f71990231a5cd61ce3a152ab841dbc270c1
Dann Blair <dann@bitspacedevelopment.com>
2016-04-09 15:33:18 -0500

    Modified the user model and added the base template

--- app/User.php
+++ app/User.php
@@ -1,26 +1,45 @@
-<?php
+<?php namespace App;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+class User extends Model implements AuthenticatableContract, CanResetPasswordContract {
+    use Authenticatable, CanResetPassword;
 
-namespace App;
+    protected $table = 'users';
 
-use Illuminate\Foundation\Auth\User as Authenticatable;
+    protected $fillable = ['name', 'email', 'password'];
 
-class User extends Authenticatable
-{
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $hidden = ['password', 'remember_token'];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    public function posts()
+    {
+        return $this->hasMany('App\Posts','author_id');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany('App\Comments','from_user');
+    }
+
+    public function can_post()
+    {
+        $role = $this->role;
+        if($role == 'author' || $role == 'admin')
+        {
+          return true;
+      }
+      return false;
+    }
+
+    public function is_admin()
+    {
+        $role = $this->role;
+        if($role == 'admin')
+        {
+          return true;
+      }
+      return false;
+    }
 }
